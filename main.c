438f9529ead7d290eb2961ceb642ccbb8a9d1493
Jdjd Gaming <80889633+valina354@users.noreply.github.com>
2025-07-05 22:50:15 +0300

    disable motionblur by default

--- main.c
+++ main.c
@@ -568,7 +568,7 @@ void init_engine(SDL_Window* window, SDL_GLContext context) {
     Cvar_Register("r_sun_shadow_distance", "50.0", "The orthographic size (radius) for the sun's shadow map frustum. Lower values = sharper shadows closer to the camera.", CVAR_NONE);
     Cvar_Register("r_texture_quality", "5", "Texture quality setting (1=very low, 2=low, 3=medium, 4=high, 5=very high).", CVAR_NONE);
     Cvar_Register("fov_vertical", "55", "The vertical field of view in degrees.", CVAR_NONE);
-    Cvar_Register("r_motionblur", "1", "Enable camera and object motion blur.", CVAR_NONE);
+    Cvar_Register("r_motionblur", "0", "Enable camera and object motion blur.", CVAR_NONE);
     Cvar_Register("g_speed", "6.0", "Player walking speed.", CVAR_NONE);
     Cvar_Register("g_sprint_speed", "8.0", "Player sprinting speed.", CVAR_NONE);
     Cvar_Register("r_debug_gi", "0", "Show G-Buffer indirect illumination.", CVAR_NONE);
