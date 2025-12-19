ddb667421dfe73c2a73cff85338da0791b134bb1
Jdjd Gaming <80889633+valina354@users.noreply.github.com>
2025-12-19 23:34:41 +0200

    Update network.c

--- engine/level1/network.c
+++ engine/level1/network.c
@@ -310,8 +310,7 @@ void Network_Init(void) {
 #ifdef PLATFORM_WINDOWS
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-        fprintf(stderr, "FATAL: WSAStartup failed.\n");
-        exit(EXIT_FAILURE);
+        Console_Printf_Error("WSAStartup failed.\n");
     }
 #endif
     Console_Printf("Network System Initialized.\n");
